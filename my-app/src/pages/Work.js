c9c52d67f0b249ed2bada0ef51f1b025b234c451
noahwx <noahfontenette@outlook.com>
2023-03-26 13:58:33 -0500

    0.0.900

--- my-app/src/pages/Work.js
+++ my-app/src/pages/Work.js
@@ -1,10 +1,34 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Work = () => {
     return ( 
         <div>
             <div className='Desktop-Wrapper'>
                 <h1 className="Page-Title">Work</h1>
+                <div className="work">
+                    <NavLink to='/' className='work-link'>
+                        <div className="work-content">
+                            <div className="work-img"></div>
+                            <h2 className="work-title">Work Title</h2>
+                            <p className="work-descrip">Another day another idk!</p>
+                        </div>
+                    </NavLink>
+                    <NavLink to='/' className='work-link'>
+                        <div className="work-content">
+                            <div className="work-img"></div>
+                            <h2 className="work-title">Work Title</h2>
+                            <p className="work-descrip">Another day another idk!</p>
+                        </div>
+                    </NavLink>
+                    <NavLink to='/' className='work-link'>
+                        <div className="work-content">
+                            <div className="work-img"></div>
+                            <h2 className="work-title">Work Title</h2>
+                            <p className="work-descrip">Another day another idk!</p>
+                        </div>
+                    </NavLink>
+                </div>
             </div>
             <div className='background'></div>
         </div>
